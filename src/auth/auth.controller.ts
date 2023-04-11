fb7d2f5925c14e220a9610a954c2d24828162e09
VictorVelozo <victor.velozo@outlook.com>
2023-04-11 18:36:59 -0300

    feat: added authentication by jwt token

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { SignInDTO } from './dtos/SignInDTO';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post()
+  signIn(@Body() signInDTO: SignInDTO) {
+    const token = this.authService.signIn(signInDTO);
+    return token;
+  }
+}
